e6dae628536efb3ab20a0a4b1e07bad608e9e350
tahaturan <tahaaturann16@gmail.com>
2023-04-11 10:15:09 +0300

    String yapisi

--- string_yapisi.playground/Contents.swift
+++ string_yapisi.playground/Contents.swift
@@ -2,3 +2,42 @@ import UIKit
 
 // String Yapisi
 
+// Tanimlama
+
+let strA = "Merhaba"
+
+let strB = String("Merhaba Nesne") // nesne olarak Tanimlama
+
+let strC = """
+ bu sekilde de
+ coklu olarak
+ sring tanimlaya biliriz
+ """
+
+let strD = String() // bos string ifade olusturma
+print(strD)
+
+print(strC)
+
+// String ifadesinin ici dolu mu bos mu kontrolu
+
+if strA.isEmpty{
+    // bos ise true degilse false doner
+    print("ici bos")
+}else{
+    print("ici dolu verisi ise: \(strA)")
+}
+
+// boyut bulma
+
+print(strA.count)
+
+// dongu ile gezinebiliriz index mantigi vardir
+
+for harf in strA{
+    print(harf , terminator: "-")
+    //parcaladiktan sonra harf sonuna isaretler ekelebiliriz terminator ile
+}
+print("----------------")
+
+
