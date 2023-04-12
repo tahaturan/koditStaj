9724f34c7cb4323f117d78a39076e10795f59969
tahaturan <tahaaturann16@gmail.com>
2023-04-12 11:50:36 +0300

    ornek layout hizlama uygulamasi

---
+++ auto_layout_uygulama/auto_layout_uygulama/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  auto_layout_uygulama
+//
+//  Created by Taha Turan on 12.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
