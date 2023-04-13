2c04094cc4c44c4318c13ba44fb1dc38732eaf6a
tahaturan <tahaaturann16@gmail.com>
2023-04-13 14:20:20 +0300

    UserlDefaults ile Veri saklama islemleri

--- BirthDayNoteTaker/BirthDayNoteTaker/ViewController.swift
+++ BirthDayNoteTaker/BirthDayNoteTaker/ViewController.swift
@@ -18,11 +18,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let myName = storedName as? String {
+            nameLabel.text = "Name: \(myName)"
+        }
+        
+        if let newBirthday = storedBirthday as? String {
+            birthdayLabel.text = "Birthday: \(newBirthday)"
+        }
     }
     
     
     @IBAction func saveButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(birthdayTextField.text, forKey: "birthday")
+        
+        
         nameLabel.text = "Name: \(nameTextField.text ?? "")"
         birthdayLabel.text = "Birthday: \( birthdayTextField.text ?? "")"
     }
