4df786f7029c23df3ec5ea76f35890f3b9b98089
tahaturan <tahaaturann16@gmail.com>
2023-04-13 11:21:07 +0300

    kod ile button olusturma

--- projectLayout/projectLayout/ViewController.swift
+++ projectLayout/projectLayout/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var myLabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,13 +17,22 @@ class ViewController: UIViewController {
         let width = view.frame.size.width
         let height = view.frame.size.height
         
-        let myLabel = UILabel()
         myLabel.text = "Test Label"
         myLabel.textAlignment = .center
-        myLabel.frame = CGRect(x: width / 2 , y: height * 0.5 , width: width * 0.8, height: 50)
+        myLabel.frame = CGRect(x: width / 2 , y: height * 0.7 , width: width * 0.8, height: 50)
         view.addSubview(myLabel)
+        
+        let myButton = UIButton()
+        myButton.setTitle("My Button", for: UIControl.State.normal)
+        myButton.setTitleColor(UIColor.blue, for: UIControl.State.normal)
+        myButton.frame = CGRect(x: width * 0.5, y: height * 0.6, width: 200, height: 100)
+        view.addSubview(myButton)
+        
+        myButton.addTarget(self, action: #selector(ViewController.myAction), for: UIControl.Event.touchUpInside)
     }
-
-
+    
+   @objc func myAction()  {
+       myLabel.text = "Tapped"
+    }
 }
 
