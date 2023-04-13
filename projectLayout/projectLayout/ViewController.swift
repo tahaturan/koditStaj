4f6d5f99727222843848c42f6aac8943c713f7ed
tahaturan <tahaaturann16@gmail.com>
2023-04-13 10:50:03 +0300

    Kodlama ile gorunum tanimlama

--- projectLayout/projectLayout/ViewController.swift
+++ projectLayout/projectLayout/ViewController.swift
@@ -11,7 +11,15 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        let myLabel = UILabel()
+        myLabel.text = "Test Label"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width / 2 , y: height * 0.5 , width: width * 0.8, height: 50)
+        view.addSubview(myLabel)
     }
 
 
