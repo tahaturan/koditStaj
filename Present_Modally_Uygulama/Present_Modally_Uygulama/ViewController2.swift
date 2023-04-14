28de370cb18575f4660c2eeade684260558e2caa
tahaturan <tahaaturann16@gmail.com>
2023-04-14 10:09:37 +0300

    Present Modally uyulama tasarim olusturuldu

---
+++ Present_Modally_Uygulama/Present_Modally_Uygulama/ViewController2.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController2.swift
+//  Present_Modally_Uygulama
+//
+//  Created by Taha Turan on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+
+}
---
+++ Present_Modally_Uygulama/Present_Modally_Uygulama/ViewController3.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController3.swift
+//  Present_Modally_Uygulama
+//
+//  Created by Taha Turan on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController3: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
