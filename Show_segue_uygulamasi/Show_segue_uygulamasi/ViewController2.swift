67d683aa8b24ad10525e15d21f4e9c1f4fd5ca45
tahaturan <tahaaturann16@gmail.com>
2023-04-14 11:56:28 +0300

    geri donusler kod ile

--- Show_segue_uygulamasi/Show_segue_uygulamasi/ViewController2.swift
+++ Show_segue_uygulamasi/Show_segue_uygulamasi/ViewController2.swift
@@ -16,14 +16,13 @@ class ViewController2: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func geri(_ sender: Any) {
+        // navigationController?.popViewController(animated: true)
+        // geri gelmesini saglayan kod
+        
+        navigationController?.popToRootViewController(animated: true)
+        // en bastaki sayfaya gider
     }
-    */
+    
 
 }
