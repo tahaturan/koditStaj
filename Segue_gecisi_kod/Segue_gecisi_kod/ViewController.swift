99a3eeab7a535df7382ab83d47497ffa8f4f22ef
tahaturan <tahaaturann16@gmail.com>
2023-04-14 14:41:22 +0300

    initial commit

---
+++ Segue_gecisi_kod/Segue_gecisi_kod/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segue_gecisi_kod
+//
+//  Created by Taha Turan on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
