569a83fa52131eee1be32e4520c2d2928a0d1579
tahaturan <tahaaturann16@gmail.com>
2023-04-17 15:48:11 +0300

    navigation controller buyuk baslik ekleme

--- navigation_controller_calismasi/navigation_controller_calismasi/ViewController.swift
+++ navigation_controller_calismasi/navigation_controller_calismasi/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         self.navigationItem.title = "baslik Kod Ile"
         
         self.navigationItem.prompt = "aciklama Kod ile"
+        self.navigationItem.largeTitleDisplayMode = .automatic
     }
 
 
