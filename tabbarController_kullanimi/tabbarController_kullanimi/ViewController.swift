31c8c19d7c3a01762a744e83917f4f0d5aabf54d
tahaturan <tahaaturann16@gmail.com>
2023-04-17 17:45:05 +0300

    tabbarController Badge Kullamimi

--- tabbarController_kullanimi/tabbarController_kullanimi/ViewController.swift
+++ tabbarController_kullanimi/tabbarController_kullanimi/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var sayac:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
 
+    @IBAction func arttir(_ sender: Any) {
+        if let tabItems = tabBarController?.tabBar.items {
+            let profileTabBarItem = tabItems[0]
+            
+            profileTabBarItem.badgeColor = .purple
+            profileTabBarItem.badgeValue = String(sayac)
+        }
 
+        sayac += 1
+        print(sayac)
+
+    }
+ 
+    
+    @IBAction func azalt(_ sender: Any) {
+        if let tabItems = tabBarController?.tabBar.items {
+            let profileTabBarItem = tabItems[0]
+            
+            profileTabBarItem.badgeColor = .purple
+            profileTabBarItem.badgeValue = String(sayac)
+        }
+        sayac -= 1
+        print(sayac)
+    }
+    
 }
 
