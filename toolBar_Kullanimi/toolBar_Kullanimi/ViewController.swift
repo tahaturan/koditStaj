3a6fa4191e7ae75d13b694bdcf44c4a5b32c3ecf
tahaturan <tahaaturann16@gmail.com>
2023-04-18 10:33:09 +0300

    ToolBar Kullanimi

--- toolBar_Kullanimi/toolBar_Kullanimi/ViewController.swift
+++ toolBar_Kullanimi/toolBar_Kullanimi/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var etiket: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func geriButton(_ sender: Any) {
+        etiket.text = "Geri"
+    }
+    
+    
+    @IBAction func oynatButton(_ sender: Any) {
+        etiket.text = "Oynat"
+    }
+    
+    
+    @IBAction func ileriButton(_ sender: Any) {
+        etiket.text = "Ileri"
+    }
+    
 }
 
