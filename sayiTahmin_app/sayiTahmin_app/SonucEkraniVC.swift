8f9727a38defc4160d3452c75eadb06e05077873
tahaturan <tahaaturann16@gmail.com>
2023-04-18 19:29:19 +0300

    Son

--- sayiTahmin_app/sayiTahmin_app/SonucEkraniVC.swift
+++ sayiTahmin_app/sayiTahmin_app/SonucEkraniVC.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class SonucEkraniVC: UIViewController {
-
+    
+    @IBOutlet weak var imageViewSonuc: UIImageView!
+    @IBOutlet weak var sonucLabel: UILabel!
+    
+    let uzgunResim = UIImage(named: "uzgun_resim")
+    let mutluResim = UIImage(named: "mutlu_resim")
+    
+    var sonuc:Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.hidesBackButton = true
+        
+        if let gelenSonuc = sonuc {
+            
+            if gelenSonuc {
+                imageViewSonuc.image = mutluResim
+                sonucLabel.text = "Kazandiniz Tebrikler"
+            } else {
+                imageViewSonuc.image = uzgunResim
+                sonucLabel.text = "Maalesef Kaybettiniz"
+            }
+        }
+        
     }
     
 
-
+    @IBAction func tekrarButton(_ sender: Any) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
 }
--- sayiTahmin_app/sayiTahmin_app/TahminEkraniVC.swift
+++ sayiTahmin_app/sayiTahmin_app/TahminEkraniVC.swift
@@ -8,13 +8,73 @@
 import UIKit
 
 class TahminEkraniVC: UIViewController {
-
+    
+    @IBOutlet weak var kalanHakLabel: UILabel!
+    @IBOutlet weak var ipUcuLabel: UILabel!
+    @IBOutlet weak var girdiTextField: UITextField!
+    
+    var rastgeleSayi:Int?
+    
+    var kalanHak:Int = 5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        rastgeleSayi = Int.random(in: 1...100)
+       
 
-        // Do any additional setup after loading the view.
+        
     }
     
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let gelenVeri = sender as? Bool {
+            let gidilecekVC  = segue.destination as! SonucEkraniVC
+            gidilecekVC.sonuc = gelenVeri
+        }
+    }
+    
+    @IBAction func tahminButton(_ sender: Any) {
+        ipUcuLabel.isHidden = false
+        kalanHak -= 1
+        
+        
+        
+        if let veri = girdiTextField.text {
+            
+            if let tahmin = Int(veri) {
+                
+                if kalanHak != 0 {
+                    
+                    if tahmin == rastgeleSayi{
+                        let sonuc:Bool = true
+                        
+                        performSegue(withIdentifier: "tahminToSonuc", sender: sonuc)
+                    }
+                    if tahmin > rastgeleSayi!{
+                        // Azalt
+                        ipUcuLabel.text = "Azalt"
+                        kalanHakLabel.text = "Kalan Hak: \(kalanHak)"
+                        
+                    }
+                    if tahmin < rastgeleSayi!{
+                        //Arttir
+                        ipUcuLabel.text = "Arttir"
+                        kalanHakLabel.text = "Kalan Hak: \(kalanHak)"
+                    }
+                    
+                }else{
+                    
+                    // Sayfa Gecisi
+                    let sonuc:Bool = false
+                    
+                    performSegue(withIdentifier: "tahminToSonuc", sender: sonuc)
+                }
+            }
+            girdiTextField.text = ""
+        }
+        
+    }
+    
 
 }
