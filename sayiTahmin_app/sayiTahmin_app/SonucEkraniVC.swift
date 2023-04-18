2b254a8be68f912eef48ae875cc969c16467bef5
tahaturan <tahaaturann16@gmail.com>
2023-04-18 18:47:49 +0300

    Tasarimlar Yapildi

---
+++ sayiTahmin_app/sayiTahmin_app/SonucEkraniVC.swift
@@ -0,0 +1,20 @@
+//
+//  SonucEkraniVC.swift
+//  sayiTahmin_app
+//
+//  Created by Taha Turan on 18.04.2023.
+//
+
+import UIKit
+
+class SonucEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ sayiTahmin_app/sayiTahmin_app/TahminEkraniVC.swift
@@ -0,0 +1,20 @@
+//
+//  TahminEkraniVC.swift
+//  sayiTahmin_app
+//
+//  Created by Taha Turan on 18.04.2023.
+//
+
+import UIKit
+
+class TahminEkraniVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
