bbebc990513754d265f017b5fe0edd2398ca8ea9
tahaturan <tahaaturann16@gmail.com>
2023-04-19 11:35:20 +0300

    Date Picker

--- picer_yapilari/picer_yapilari/ViewController.swift
+++ picer_yapilari/picer_yapilari/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tarihTexfield: UITextField!
+    @IBOutlet weak var saatTextfield: UITextField!
+    
+    var datePicker: UIDatePicker?
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        datePicker = UIDatePicker()
+        datePicker?.datePickerMode = .date
+        tarihTexfield.inputView = datePicker
+        
+        datePicker?.addTarget(self, action: #selector(self.tarihGoster(datePicker:)), for: .valueChanged)
+        
+        
+    }
+    
+  @objc func tarihGoster(datePicker:UIDatePicker)  {
+      
+      let dateFormater = DateFormatter()
+      
+      dateFormater.dateFormat = "MM/dd/yyyy"
+      let alinanTarih = dateFormater.string(from: datePicker.date)
+      
+      tarihTexfield.text = alinanTarih
+      
+      view.endEditing(true)
     }
 
 
