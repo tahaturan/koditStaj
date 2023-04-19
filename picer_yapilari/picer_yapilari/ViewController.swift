6314465b81469ea493cc749490951f71448a7fb4
tahaturan <tahaaturann16@gmail.com>
2023-04-19 12:23:19 +0300

    Time Picker Kullanimi ve kodun parcalama islemleri

--- picer_yapilari/picer_yapilari/ViewController.swift
+++ picer_yapilari/picer_yapilari/ViewController.swift
@@ -13,31 +13,50 @@ class ViewController: UIViewController {
     @IBOutlet weak var saatTextfield: UITextField!
     
     var datePicker: UIDatePicker?
+    var timePicker:UIDatePicker?
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //DatePicker
+        datePickerShow()
+        //TimePicker
+        timePickerShow()
+
+    }
+    
+    func datePickerShow()  {
         datePicker = UIDatePicker()
         datePicker?.datePickerMode = .date
         tarihTexfield.inputView = datePicker
         
         datePicker?.addTarget(self, action: #selector(self.tarihGoster(datePicker:)), for: .valueChanged)
-        
-        
     }
     
-  @objc func tarihGoster(datePicker:UIDatePicker)  {
-      
-      let dateFormater = DateFormatter()
-      
-      dateFormater.dateFormat = "MM/dd/yyyy"
-      let alinanTarih = dateFormater.string(from: datePicker.date)
-      
-      tarihTexfield.text = alinanTarih
-      
-      view.endEditing(true)
+    func timePickerShow()  {
+        timePicker = UIDatePicker()
+        timePicker?.datePickerMode = .time
+        saatTextfield.inputView = timePicker
+        timePicker?.addTarget(self, action: #selector(self.saatGoster(datePicker:)), for: .valueChanged)
     }
+    
+    @objc func tarihGoster(datePicker:UIDatePicker)  {
+        let dateFormater = DateFormatter()
+        dateFormater.dateFormat = "MM/dd/yyyy"
+        let alinanTarih = dateFormater.string(from: datePicker.date)
+        tarihTexfield.text = alinanTarih
+        
+        view.endEditing(true)
+      }
+      
+     @objc func saatGoster(datePicker:UIDatePicker)  {
+         let dateFormater = DateFormatter()
+         dateFormater.dateFormat = "hh:mm"
+         let alinanSaat = dateFormater.string(from: datePicker.date)
+         saatTextfield.text = alinanSaat
+      
+      }
 
 
 }
