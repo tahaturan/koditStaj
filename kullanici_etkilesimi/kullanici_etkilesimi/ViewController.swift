f746ba4aacc3e6db1a281ef5e40b870240325df7
tahaturan <tahaaturann16@gmail.com>
2023-04-19 10:17:04 +0300

    Ozellestirilmis Alert Kullanimi

--- kullanici_etkilesimi/kullanici_etkilesimi/ViewController.swift
+++ kullanici_etkilesimi/kullanici_etkilesimi/ViewController.swift
@@ -40,6 +40,48 @@ class ViewController: UIViewController {
     
     
     @IBAction func ozelAlertGoster(_ sender: Any) {
+        let alertController = UIAlertController(title: "Baslik", message: "Mesaj", preferredStyle: .alert)
+        
+        alertController.addTextField{
+            textfield in
+            textfield.placeholder = "E-mail"
+            textfield.keyboardType = .emailAddress
+            
+        }
+        
+        alertController.addTextField{
+            textfield in
+            textfield.placeholder = "Sifre"
+            textfield.isSecureTextEntry = true
+        }
+        
+        let kaydetAction = UIAlertAction(title: "Kaydet", style: .destructive){
+            action in
+            
+            print("Kaydedildi")
+            
+            var alinanEmail:String = ""
+            var alinanSifre:String = ""
+            
+            if let eMail = alertController.textFields?[0]  {
+                if let mail = eMail.text {
+                    alinanEmail = mail
+                }
+            }
+            
+            if let sifre = alertController.textFields?[1] {
+                if let gelenSifre = sifre.text {
+                    alinanSifre = gelenSifre
+                }
+            }
+            
+            self.labelSonuc.text = "Email: \(alinanEmail) - Sifre: \(alinanSifre)"
+    
+        }
+        alertController.addAction(kaydetAction)
+        self.present(alertController, animated: true)
+        
+        
     }
     
     
