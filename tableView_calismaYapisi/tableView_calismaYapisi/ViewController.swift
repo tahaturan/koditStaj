4fbdea551fde4960201e114dd55670fb62e5623a
tahaturan <tahaaturann16@gmail.com>
2023-04-19 16:20:49 +0300

    basit TableView Kullanimi

--- tableView_calismaYapisi/tableView_calismaYapisi/ViewController.swift
+++ tableView_calismaYapisi/tableView_calismaYapisi/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ulkeler:[String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ulkeler = ["Turkiye" , "Italya" , "Almanya" , "Japonya" , "Rusya" , "Fransa" , "Misir"]
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ulkeler.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "hucreadi", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = ulkeler[indexPath.row]
+        cell.contentConfiguration = content
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("secilen ulke: \(ulkeler[indexPath.row])")
     }
 
 
