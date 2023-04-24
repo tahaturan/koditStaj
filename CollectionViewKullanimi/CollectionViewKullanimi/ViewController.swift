35d39796aa87ac25cf1d6dd6e9b5a05b6e3be8e2
tahaturan <tahaaturann16@gmail.com>
2023-04-24 17:34:10 +0300

    initial commit

---
+++ CollectionViewKullanimi/CollectionViewKullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollectionViewKullanimi
+//
+//  Created by Taha Turan on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
