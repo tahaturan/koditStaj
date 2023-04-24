601f5cd4eaf216a85142db4f518b4b09ce1eb24a
tahaturan <tahaaturann16@gmail.com>
2023-04-24 17:57:04 +0300

    CollectionView kullanimi

---
+++ CollectionViewKullanimi/CollectionViewKullanimi/CollectionViewHucre.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewHucre.swift
+//  CollectionViewKullanimi
+//
+//  Created by Taha Turan on 24.04.2023.
+//
+
+import UIKit
+
+class CollectionViewHucre: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var labelUlkeAdi: UILabel!
+    
+}
--- CollectionViewKullanimi/CollectionViewKullanimi/ViewController.swift
+++ CollectionViewKullanimi/CollectionViewKullanimi/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var ulkeler:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        ulkeler = ["Tukiye" , "Almanya" , "Japonya" , "Italya" , "Rusya" , "Romanya" , "Fransa"]
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
-
 
 }
 
+
+extension ViewController: UICollectionViewDelegate , UICollectionViewDataSource{
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return ulkeler.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ulkeHucre", for: indexPath) as! CollectionViewHucre
+        
+        cell.labelUlkeAdi.text = ulkeler[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(ulkeler[indexPath.row]) secildi")
+    }
+    
+}
