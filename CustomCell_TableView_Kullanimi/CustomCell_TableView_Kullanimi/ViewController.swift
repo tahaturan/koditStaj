45eff858599b67be14f91a65d1b3bbe3e97a1d2c
tahaturan <tahaaturann16@gmail.com>
2023-04-24 15:22:54 +0300

    initial Commit Custom Cell Kullanimi

---
+++ CustomCell_TableView_Kullanimi/CustomCell_TableView_Kullanimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomCell_TableView_Kullanimi
+//
+//  Created by Taha Turan on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
