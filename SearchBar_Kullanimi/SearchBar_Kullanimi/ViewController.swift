351ff30dcff7eb605c72be1aec66bff5adb84889
tahaturan <tahaaturann16@gmail.com>
2023-04-25 13:55:40 +0300

    SearchBar Kullanimi

--- SearchBar_Kullanimi/SearchBar_Kullanimi/ViewController.swift
+++ SearchBar_Kullanimi/SearchBar_Kullanimi/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+        
+        
     }
 
 
 }
 
+extension ViewController: UISearchBarDelegate{
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("arama sonuc : \(searchText)")
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print("Iptal")
+    }
+    
+    
+}
