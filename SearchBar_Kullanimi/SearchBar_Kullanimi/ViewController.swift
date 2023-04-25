43d825c49c732e869663f1de8bad7266a28d31fe
tahaturan <tahaaturann16@gmail.com>
2023-04-25 14:26:40 +0300

    TableView icinde SearchBar kullanimi

--- SearchBar_Kullanimi/SearchBar_Kullanimi/ViewController.swift
+++ SearchBar_Kullanimi/SearchBar_Kullanimi/ViewController.swift
@@ -12,9 +12,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var ulkeler:[String] = []
+    var aramaSonucuUlkeler:[String] = []
+    var aramaYapiliyorMu:Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        ulkeler = ["Turkiye" , "Almanya" , "Rusya" , "Italya" , "Fransa" , "Romanya"]
         
         
     }
@@ -22,15 +34,80 @@ class ViewController: UIViewController {
 
 }
 
+//SearchBar Extension
 extension ViewController: UISearchBarDelegate{
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print("arama sonuc : \(searchText)")
+        
+        
+        if searchText == ""{
+            aramaYapiliyorMu = false
+        }else{
+            aramaYapiliyorMu = true
+            aramaSonucuUlkeler = ulkeler.filter({$0.lowercased().contains(searchText.lowercased())})
+        }
+        
+      
+        
+        tableView.reloadData()
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        print("Iptal")
+        
     }
     
     
+}
+
+//TableView Extension
+
+extension ViewController: UITableViewDelegate , UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if aramaYapiliyorMu{
+            return aramaSonucuUlkeler.count
+        }else{
+            return ulkeler.count
+        }
+        
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ulkelerHucre", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+      
+        
+        if aramaYapiliyorMu {
+            content.text = aramaSonucuUlkeler[indexPath.row]
+            cell.contentConfiguration = content
+        } else {
+            content.text = ulkeler[indexPath.row]
+            cell.contentConfiguration = content
+        }
+        
+        return cell
+        
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if aramaYapiliyorMu {
+            let secilenUlke = aramaSonucuUlkeler[indexPath.row]
+            
+            print("\(secilenUlke) secildi")
+        } else {
+            let secilenUlke = ulkeler[indexPath.row]
+            
+            print("\(secilenUlke) secildi")
+        }
+    }
 }
