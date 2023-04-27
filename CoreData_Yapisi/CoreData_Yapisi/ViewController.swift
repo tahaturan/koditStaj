c1924cfa0e0c1483f40861c64c8c37acf44f8929
tahaturan <tahaaturann16@gmail.com>
2023-04-27 11:15:41 +0300

    initial commit

---
+++ CoreData_Yapisi/CoreData_Yapisi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData_Yapisi
+//
+//  Created by Taha Turan on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
